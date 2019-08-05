319baabcb39965cdcf3b8bc87e19c4d42ed89caf
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-08-05 14:33:38 -0600

    Added pop-ups for end of game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,8 @@
+//  Variable that will hold our setInterval
+var intervalId;
+var clockInterval;
+var index = 0;
+
 var triviagame = {
     correct: 0,
     wrong: 0,
@@ -11,107 +16,112 @@ var triviagame = {
         },
         {
             qid: 1,
-            question:  "Which national park is the largest acording to size?",
+            question: "Which national park is the largest acording to size?",
             answers: ["Acadia", "Grand Canyon", "Yellowstone", "Wrangell-St. Elias"],
             correctAnswer: 3
         },
         {
             qid: 2,
-            question:  "Which continent hosts the Atlas Mountains?",
+            question: "Which continent hosts the Atlas Mountains?",
             answers: ["Europe", "Africa", "Asia", "Australia"],
             correctAnswer: 1
         },
         {
             quid: 3,
-            question:  "The highest peaks in the Western Hemisphere are found in which mountain range?",
+            question: "The highest peaks in the Western Hemisphere are found in which mountain range?",
             answers: ["The Rocky Mountains", "The Andes Mountain", "The Sierra Madres", "The Sierra Nevadas"],
             correctAnswer: 1
         },
-        
+
     ]
 }
-//  Variable that will hold our setInterval
-    var intervalId;
-    var clockInterval;
-    var index = 0;
 
-  function displayQuestions() { 
-    var correct = '';
+
+function displayQuestions() {
+    var correctText = '';
+    console.log("I'm in display");
+    
     $("#question").text(triviagame.questionsArray[index].question);
     $("#answers").empty();
 
-    for (let i = 0; i < triviagame.questionsArray[index].answers.length; i++) {      
+    for (let i = 0; i < triviagame.questionsArray[index].answers.length; i++) {
         const questionId = triviagame.questionsArray[index].qid;
         const correctAnswer = triviagame.questionsArray[index].correctAnswer;
         const element = triviagame.questionsArray[index].answers[i];
-        correct = triviagame.questionsArray[index].answers[correctAnswer];
-        $("#answers").append("<button type='button' class='btn btn-light btn-med btn-block' data-correct='"+correctAnswer+"' id='"+i+"'>" + element + "</button>");
+        correctText = triviagame.questionsArray[index].answers[correctAnswer];
+        $("#answers").append("<button type='button' class='btn btn-light btn-med btn-block' data-correct='" + correctAnswer + "' id='" + i + "'>" + element + "</button>");
     }
+
+    $("button").on("click", function () {
+        clear();
+        console.log("the button id " + $(this).attr('id'));
+        console.log("the button correct " + $(this).attr('data-correct'));
     
-    $("button").on("click", function () {  
-        console.log("the button id "+$(this).attr('id'));
-        console.log("the button correct "+$(this).attr('data-correct'));
-        
-        if($(this).attr('id') === $(this).attr('data-correct')){
+        if ($(this).attr('id') === $(this).attr('data-correct')) {
             triviagame.correct++;
-            setTimeout(displayCongratsAnswer(correct), 1000);    
-            run();
+            setTimeout(displayAnswer(correctText, "congrats"), 1000);
         } else {
             triviagame.wrong++;
-            setTimeout(displayWrongAnswer(correct), 1000);           
-            run();
+            setTimeout(displayAnswer(correctText, "bummer"), 1000);
         }
-     });
 
-     if (index === triviagame.questionsArray.length -1){
-        stop();
+        index++;
+
+    if(index < triviagame.questionsArray.length){
+        run();       
+    }
+    else {
+        clear();
+        setTimeout(displayAnswer(correctText, "end"), 1000);
+    }
+    });
+    
+}
+
+function displayAnswer(correct, type) {
+
+    $("#answers").empty();
+    if(type == "end"){
+        $("#answers").append("<p class='card-text'>The End!");
+    }
+    if (type === "congrats") {
+        $("#answers").append("<p class='card-text'>Congrats!");
+        $("#correct").text("Correct: " + triviagame.correct);
+    }
+    else {
+        $("#answers").append("<p class='card-text'>Bummer Wrong Answer!");
+        $("#wrong").text("Wrong: " + triviagame.wrong);
     }
 
-  }
-  function displayCongratsAnswer(correct){
-    $("#answers").empty();
-    $("#answers").append("<p class='card-text'>Congrats!");
-    $("#answers").append("Correct answer <BR>"+correct+"</p>");
-    $("#correct").text("Correct: "+triviagame.correct);
-    
-  }
+    $("#answers").append("<BR>The correct answer is <BR> <font color='red'>" + correct + "</font><BR>");
 
-  function displayWrongAnswer(correct){
-    $("#answers").empty();
-    $("#answers").append("<span font='red'>Bummer Wrong Answer!</span>");
-    $("#answers").append("<BR>Correct answer <BR>"+correct+"<BR>");
-    $("#wrong").text("Wrong: "+triviagame.wrong);
-  }
+}
 
-  function run() {
+function run() {
+    clear();
+    startTimer();
+    intervalId = setInterval(displayQuestions, 10 * 1000);
+
+}
+
+function clear() {
     clearInterval(intervalId);
     clearInterval(clockInterval);
-    intervalId = setInterval(displayQuestions, 20 * 1000);  
-    startTimer(); 
-    index++;
-  }
+}
 
-  function stop() {
-    clearInterval(intervalId);
-    clearInterval(clockInterval);
-  }
-
-  function startTimer(){
-    var seconds = 20;
+function startTimer() {
+    var seconds = 10;
     clockInterval = setInterval(function () {
         $("#clock").html(seconds);
         seconds--;
-        if(seconds === 0){
-            seconds = 20;
+        if (seconds === 0) {
+            seconds = 10;
         }
     }, 1000);
-  }
+}
 
-  displayQuestions();
-  run();
 
- 
 
- 
+displayQuestions();
 
- 
+run();
