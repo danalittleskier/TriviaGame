0d0c6f12045251aa24e6fc13a1016e2449293a9d
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-08-05 11:41:12 -0600

    Changed the displayQuestions function to pass the correct answer in the button. changed the onclick button event to be in the function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,24 +4,28 @@ var triviagame = {
 
     questionsArray: [
         {
+            qid: 0,
             question: "What is the tallest mountain?",
             answers: ["Kilimanjaro", "Matterhorn", "Everest", "Dolomites"],
-            correctAnswer: "Everest"
+            correctAnswer: 2
         },
         {
+            qid: 1,
             question:  "Which national park is the largest acording to size?",
             answers: ["Acadia", "Grand Canyon", "Yellowstone", "Wrangell-St. Elias"],
-            correctAnswer: "Wrangell-St. Elias"
+            correctAnswer: 3
         },
         {
+            qid: 2,
             question:  "Which continent hosts the Atlas Mountains?",
             answers: ["Europe", "Africa", "Asia", "Australia"],
-            correctAnswer: "Africa"
+            correctAnswer: 1
         },
         {
+            quid: 3,
             question:  "The highest peaks in the Western Hemisphere are found in which mountain range?",
             answers: ["The Rocky Mountains", "The Andes Mountain", "The Sierra Madres", "The Sierra Nevadas"],
-            correctAnswer: "The Andes Mountain"
+            correctAnswer: 1
         },
         
     ]
@@ -31,47 +35,45 @@ var triviagame = {
     var clockInterval;
     var index = 0;
 
-//  This code will run as soon as the page loads.
-window.onload = function() {
-    displayQuestions();
-    run();
-
-    $("button").on("click", function () {  
-        console.log("clicked button");
+  function displayQuestions() { 
+    if (index === triviagame.questionsArray.length -1){
         stop();
-        if($(this).text === triviagame.questionsArray[index].correctAnswer){
+    }
+    $("#question").text(triviagame.questionsArray[index].question);
+    $("#answers").empty();
+
+    for (let i = 0; i < triviagame.questionsArray[index].answers.length; i++) {      
+        const questionId = triviagame.questionsArray[index].qid;
+        const correctAnswer = triviagame.questionsArray[index].correctAnswer;
+        const element = triviagame.questionsArray[index].answers[i];
+        $("#answers").append("<button type='button' class='btn btn-light btn-med btn-block' data-correct='"+correctAnswer+"' id='"+i+"'>" + element + "</button>");
+    }
+    
+    $("button").on("click", function () {  
+        console.log("the button id "+$(this).attr('id'));
+        console.log("the button correct "+$(this).attr('data-correct'));
+        stop();
+        
+        if($(this).attr('id') === $(this).attr('data-correct')){
             triviagame.correct++;
             console.log("One more correct");
             $("#correct").text("Correct: "+triviagame.correct);
             run();
         } else {
-            console.log("One more incorrect");
+            console.log("One more incorrect ");
             triviagame.wrong++;
             $("#wrong").text("Wrong: "+triviagame.wrong);
             run();
         }
      });
-  };
 
-
-  function displayQuestions() { 
-    $("#question").text(triviagame.questionsArray[index].question);
-    $("#answers").empty();
-    for (let i = 0; i < triviagame.questionsArray[index].answers.length; i++) {
-        const element = triviagame.questionsArray[index].answers[i];
-        $("#answers").append("<button type='button' class='btn btn-light btn-med btn-block' id='"+i+"'>" + element + "</button>");
-    }
-
-    console.log(triviagame.questionsArray.length);
-    if (index === triviagame.questionsArray.length -1){
-        stop();
-    }
-
-    index++;  
+    
+    index++;
   }
 
   function run() {
     clearInterval(intervalId);
+    clearInterval(clockInterval);
     intervalId = setInterval(displayQuestions, 20 * 1000);  
     startTimer(); 
   }
@@ -91,3 +93,12 @@ window.onload = function() {
         }
     }, 1000);
   }
+
+  displayQuestions();
+  run();
+
+ 
+
+ 
+
+ 
